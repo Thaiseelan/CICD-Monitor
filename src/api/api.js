54b1e35f46f416d5e020c19d8d6b69cc09c37d93
Thaiseelan <thaiseelan@gmail.com>
2026-03-24 09:23:26 +0530

    Update frontend API baseURL to Render backend deployment

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://my-project-backend-ai5r.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
